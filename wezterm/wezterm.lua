b5c8fdb2a93cbe7d6399aefdcfea1d02bce25603
luokai <kaiv2@outlook.com>
2022-07-07 10:08:22 +0800

    全屏配置

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,19 @@
 local wezterm = require("wezterm")
 return {
 	color_scheme = "Gruvbox Dark",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 
 	font_size = 13,
 	font = wezterm.font_with_fallback({
 		"CaskaydiaCove Nerd Font Mono",
 		"Cascadia Mono PL",
 	}),
+	keys = {
+		{ key = "n", mods = "SHIFT|CTRL", action = wezterm.action.ToggleFullScreen },
+	},
 }
