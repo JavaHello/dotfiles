51e6bbb5a4b211c3fce8cf9f1d473c08c7daaee0
luokai <kaiv2@outlook.com>
2023-02-14 10:57:28 +0800

    config font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,22 @@
 local wezterm = require('wezterm')
+-- wezterm.target_triple
+-- x86_64-pc-windows-msvc - Windows
+-- x86_64-apple-darwin - macOS (Intel)
+-- aarch64-apple-darwin - macOS (Apple Silicon)
+-- x86_64-unknown-linux-gnu - Linux
+local function fonts()
+  local font = {
+    'Hack Nerd Font Mono',
+  }
+  if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    table.insert(font, 'Noto Sans Mono CJK SC')
+  elseif wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+    table.insert(font, 'Noto Sans Mono CJK SC')
+  else
+    table.insert(font, 'PingFang SC')
+  end
+  return font
+end
 return {
   color_scheme = 'Gruvbox Dark',
   -- color_scheme = "gruvbox_material_dark_hard",
@@ -45,10 +63,7 @@ return {
   -- window_decorations = 'NONE',
 
   font_size = 13,
-  font = wezterm.font_with_fallback({
-    'Hack Nerd Font Mono',
-    'Noto Sans Mono CJK SC',
-  }),
+  font = wezterm.font_with_fallback(fonts()),
   keys = {
     { key = 'Enter', mods = 'CMD', action = wezterm.action.ToggleFullScreen },
   },
