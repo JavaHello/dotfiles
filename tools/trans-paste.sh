d0e147770e1de2b08cf20022961da3190d4ca365
luokai <kaiv2@outlook.com>
2023-06-23 17:13:12 +0800

    update

--- tools/trans-paste.sh
+++ tools/trans-paste.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # notify-send "$(trans en:zh "$(wl-paste)" | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g')"
-notify-send "$(trans -proxy 127.0.0.1:1087 -host zh -t zh+en "$(wl-paste)" -no-ansi)"
+notify-send "$(trans -proxy 127.0.0.1:1087 -host zh -t zh+en "$(wl-paste --primary)" -no-ansi)"
